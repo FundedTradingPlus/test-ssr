969ba487308f3613ffdbce1629af7e9d22ef77e7
Piotr Grochowski <piotr@fundedtradingplus.com>
2026-01-21 14:55:46 +0000

    bg colour

--- app/components/NavigationStatic.tsx
+++ app/components/NavigationStatic.tsx
@@ -1,6 +1,6 @@
 export default function NavigationStatic() {
   return (
-    <nav className="bg-white dark:bg-zinc-900 shadow-lg">
+    <nav className="dark:bg-zinc-900 shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
